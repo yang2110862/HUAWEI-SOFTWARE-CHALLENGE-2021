7402347280a7ab116f2956a9f6806c224614ea32
yang2110862 <1563782600@qq.com>
2021-03-24 14:53:35 +0800

    base

---
+++ HUAWEI/main.cpp
@@ -0,0 +1,29 @@
+#include "H.h"
+
+// #define TEST
+void ParseInput() {
+    int server_nums;
+    cin << server_nums;
+    string 
+}
+
+void SolveProblem() {
+    //traverse day nums;
+    //traverse request
+
+    if (/*add*/) {
+
+    } else {  // del
+
+    }
+}
+
+void Print() {
+
+}
+int main(int argc, char* argv[]) {
+    ParseInput();
+    SolveProblem();
+    Print();
+    return 0;
+}
