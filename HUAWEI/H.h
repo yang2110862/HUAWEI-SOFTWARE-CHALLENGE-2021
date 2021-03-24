151e78c4408028378059cc074990699ba53f6869
yang2110862 <1563782600@qq.com>
2021-03-24 21:13:59 +0800

    ParseInput

--- HUAWEI/H.h
+++ HUAWEI/H.h
@@ -4,31 +4,33 @@
 #include <vector>
 #include <unordered_map>
 #include <ctime>
+#include <fstream>
 using namespace std;
-/*例如(NV603, 92, 324, 53800, 500)表示一种服务
-器类型，其型号为 NV603，包含 92 个 CPU 核心， 324G 内存，硬件成本为
-53800，每日能耗成本为 500。*/
 struct SoldServer {   
-    string type;
+    string server_type;
     int cpu_cores;
-    int memory_sizes;
+    int memory_size;
     int hardware_cost;
     int daily_energy_cost;
 };
-/*例如(s3.small.1, 1, 1, 0)表示一种虚拟机类型，其型号为
-s3.small.1，所需 CPU 核数为 1，所需内存为 1G，单节点部署； (c3.8xlarge.2, 32,
-64, 1)表示一种虚拟机类型，其型号为 c3.8xlarge.2，所需 CPU 核数为 32，所需内
-存为 64G，双节点部署。 */
 struct SoldVM {
-    string type;
+    //string VM_type;
     int cpu_cores;
-    int memory_sizes;
+    int memory_size;
     int deployment_way;
 };
-/*例如(add, c3.large.4, 1)表示创建一台型号为 c3.large.4， ID 为 1 的虚拟机； (del, 1)
-表示删除 ID 为 1 的虚拟机。*/
 struct RequestData {
     string operation;
     string VM_type;
-    string VM_ID;
+    int VM_ID;
+};
+struct PurchasedServer {
+    string server_name;
+    int server_ID;
+    int total_core_num;
+    int total_memory_size;
+    int A_remain_core_num;
+    int A_remain_memory_size;
+    int B_remain_core_num;
+    int B_remain_memory_size;
 };
--- HUAWEI/main.cpp
+++ HUAWEI/main.cpp
@@ -1,15 +1,104 @@
 #include "H.h"
+//#define TEST_PARSEINPUT
+#define REDIRECT
+vector<SoldServer> sold_servers;
+unordered_map<string, SoldVM> VM_type2info;
+vector<vector<RequestData>> request_datas;
 
-// #define TEST
+void ParseServerInfo() {
+    int server_num;
+    cin >> server_num;
+    string server_type, cpu_core, memory_size, hardware_cost, daily_energy_cost;
+    for (int i = 0; i < server_num; ++i) {
+        SoldServer sold_server;
+        cin >> server_type >> cpu_core >> memory_size >> hardware_cost >> daily_energy_cost;
+        sold_server.server_type = server_type.substr(1, server_type.size() - 2);
+        sold_server.cpu_cores = stoi(cpu_core.substr(0, cpu_core.size() - 1)) / 2;
+        sold_server.memory_size = stoi(memory_size.substr(0, memory_size.size() - 1)) / 2;
+        sold_server.hardware_cost = stoi(hardware_cost.substr(0, hardware_cost.size() - 1));
+        sold_server.daily_energy_cost = stoi(daily_energy_cost.substr(0, daily_energy_cost.size() - 1));   
+        sold_servers.emplace_back(sold_server);   //可优化，不需要sold_server
+    }
+}
+void ParseVmInfo() {
+    int VM_num;
+    cin >> VM_num;
+    string VM_type, cpu_core, memory_size, deployment_way;
+    for (int i = 0; i < VM_num; ++i) {
+        SoldVM sold_VM;
+        cin >> VM_type >> cpu_core >> memory_size >> deployment_way;
+        VM_type = VM_type.substr(1, VM_type.size() - 2);
+        sold_VM.cpu_cores = stoi(cpu_core.substr(0, cpu_core.size() - 1));
+        sold_VM.memory_size = stoi(memory_size.substr(0, memory_size.size() - 1));
+        sold_VM.deployment_way = stoi(deployment_way.substr(0, deployment_way.size() - 1));
+        if (sold_VM.deployment_way == 1) {
+            sold_VM.cpu_cores /= 2;
+            sold_VM.memory_size /= 2;
+        }
+        VM_type2info[VM_type] = sold_VM;
+    }
+}
+void ParseRequest() {
+    int day_num;
+    cin >> day_num;
+    int operation_num;
+    string operation, VM_type, VM_ID;
+    for (int i = 0; i < day_num; ++i) {
+        cin >> operation_num;
+        vector<RequestData> request_data;
+        for (int j = 0; j < operation_num; ++j) {
+            cin >> operation;
+            operation = operation.substr(1, operation.size() - 2);
+            if (operation == "add") {
+                RequestData data;
+                data.operation = "add";
+                cin >> VM_type >> VM_ID;
+                data.VM_type = VM_type.substr(0, VM_type.size() - 1);
+                data.VM_ID = stoi(VM_ID.substr(0, VM_ID.size() - 1));
+                request_data.emplace_back(data);
+            } else if (operation == "del") {
+                RequestData data;
+                data.operation = "del";
+                cin >> VM_ID;
+                data.VM_ID = stoi(VM_ID.substr(0, VM_ID.size() - 1));
+                request_data.emplace_back(data);
+            }
+        }
+        request_datas.emplace_back(request_data);
+    }
+}
 void ParseInput() {
-    int server_nums;
-    cin << server_nums;
-    string 
+    ParseServerInfo();
+    ParseVmInfo();
+    ParseRequest();
+#ifdef TEST_PARSEINPUT
+    cout << sold_servers.size() << endl;
+    for (auto sold_server : sold_servers) {
+        cout << sold_server.server_type << " " << sold_server.cpu_cores << " " << sold_server.memory_size 
+        << " " << sold_server.hardware_cost << " " << sold_server.daily_energy_cost << endl;
+    }
+    cout << VM_type2info.size() << endl;
+    for (auto vm : VM_type2info) {
+        cout << vm.first << " " << vm.second.cpu_cores << " " << vm.second.memory_size << " "
+        << vm.second.deployment_way << endl;
+    }
+    cout << request_datas.size() << endl;
+    for (auto request : request_datas) {
+        cout << request.size() << endl;
+        for (auto data : request) {
+            if (data.operation == "add") {
+                cout << data.operation << " " << data.VM_type << " " << data.VM_ID << endl;
+            } else if (data.operation == "del") {
+                cout << data.operation << " " << data.VM_ID << endl;
+            } else {
+                cerr << "operation error!";
+            }
+        }
+    }
+#endif
 }
 
 void SolveProblem() {
-    //traverse day nums;
-    //traverse request
 
     if (/*add*/) {
 
@@ -22,8 +111,18 @@ void Print() {
 
 }
 int main(int argc, char* argv[]) {
+#ifdef REDIRECT
+    freopen("train.txt", "r", stdin);
+    freopen("out.txt", "w", stdout); 
+#endif
+
     ParseInput();
     SolveProblem();
-    Print();
+    //Print();
+
+#ifdef REDIRECT
+    fclose(stdin);
+    fclose(stdout);
+#endif
     return 0;
 }
