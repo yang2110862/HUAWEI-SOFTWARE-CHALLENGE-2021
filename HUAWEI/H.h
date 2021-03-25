f3daf5fce84774f7ee6286473e259f83f6c75245
yang2110862 <1563782600@qq.com>
2021-03-25 11:20:47 +0800

    base

--- HUAWEI/H.h
+++ HUAWEI/H.h
@@ -5,39 +5,50 @@
 #include <unordered_map>
 #include <ctime>
 #include <fstream>
+#include <unordered_set>
 using namespace std;
 
 //#define TEST_PARSEINPUT
 #define REDIRECT
 
 struct SoldServer {   
-    string server_type;
+    string server_name;
     int cpu_cores;
     int memory_size;
     int hardware_cost;
     int daily_energy_cost;
 };
-struct SoldVM {
-    //string VM_type;
+struct SoldVm {
+    //string VM_name;
     int cpu_cores;
     int memory_size;
     int deployment_way;
 };
 struct RequestData {
     string operation;
-    string VM_type;
-    int VM_ID;
+    string vm_name;
+    int vm_id;
 };
 struct PurchasedServer {
     string server_name;
-    int server_ID;
+    int server_id;
     int total_core_num;
     int total_memory_size;
     int A_remain_core_num;
     int A_remain_memory_size;
     int B_remain_core_num;
     int B_remain_memory_size;
+    unordered_set<int> a_vm_id;
+    unordered_set<int> b_vm_id;
+    unordered_set<int> ab_vm_id;
 };
+struct VmIdInfo {
+    PurchasedServer* purchase_server;
+    string node;  //A 代表A节点  B代表B节点  C代表C节点
+    string vm_name;
+    int cpu_cores;
+    int memory_size;
+}
 
 class Evaluate {
 public:
