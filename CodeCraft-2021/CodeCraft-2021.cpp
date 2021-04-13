b3e8dcb31a647dff49acead41814140353190a1a
yang2110862 <1563782600@qq.com>
2021-04-13 16:18:38 +0800

    base

--- CodeCraft-2021/CodeCraft-2021.cpp
+++ CodeCraft-2021/CodeCraft-2021.cpp
@@ -1,12 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2021-04-04 19:30:29
- * @LastEditTime: 2021-04-13 15:59:21
+ * @LastEditTime: 2021-04-13 16:09:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \planC\CodeCraft-2021\CodeCraft-2021.cpp
  */
 #include "data.h"
+#include "scheduler.h"
 unordered_map<string, SoldServer> server_info;
 unordered_map<string, SoldVm> VM_info;
 queue<vector<RequestData>> request_datas;
@@ -16,5 +17,6 @@ int main() {
     Parse parse;
     parse.parse_input(server_info, VM_info, request_datas);
     //parse.print(server_info, VM_info, request_datas);
+    
     return 0;
 }
--- CodeCraft-2021/scheduler.h
+++ CodeCraft-2021/scheduler.h
@@ -1,19 +1,32 @@
 /*
  * @Author: your name
  * @Date: 2021-04-04 19:30:43
- * @LastEditTime: 2021-04-13 15:14:07
+ * @LastEditTime: 2021-04-13 16:16:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \planC\CodeCraft-2021\H2.h
  */
 #include "data.h"
 #ifndef SCHEDULER_H
-#define SCH
+#define SCHEDULER_H
 
-class H2 {
+extern int total_days_num;  
+extern int foreseen_days_num;
+class Scheduler {
 public:
-    int x = 3;
+    Scheduler() {}
+    ~Scheduler() {}
+    void solve_problem();
+private: 
+    void simulate_deploy();
+    void purchase();
+    void deployment();
 };
+void Scheduler::solve_problem() {
+    for (int i = 0; i < total_days_num; ++i) {
+        
+    }
+}
 
 
 #endif
